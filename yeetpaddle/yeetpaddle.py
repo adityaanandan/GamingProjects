96b3ae7d838c313b7f4add572657a3ad21d076af
EpicGamerAditya <66402720+EpicGamerAditya@users.noreply.github.com>
2020-06-03 21:50:48 -0700

    Create yeetpaddle.py

---
+++ yeetpaddle/yeetpaddle.py
@@ -0,0 +1,19 @@
+import pygame
+from random import randint
+pygame.init()
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+class Paddle(pygame.sprite.Sprite):
+    def __init__(self, color, width, height):
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+        self.rect = self.image.get_rect()
+    def moveright(self, velocity):
+        self.rect.x += velocity
+    
+    def moveleft(self, velocity):
+        self.rect.x -= velocity
+        
