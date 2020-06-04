d7ed0fe4b5ab519dc75cf5d3a62db149526c5d02
EpicGamerAditya <66402720+EpicGamerAditya@users.noreply.github.com>
2020-06-03 21:50:04 -0700

    Create yeetball.py

---
+++ yeetpaddle/yeetball.py
@@ -0,0 +1,23 @@
+import pygame
+from random import randint
+pygame.init()
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, color, width, height):
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+        self.velocity = 5
+        self.rect = self.image.get_rect()
+    def update(self):
+        self.rect.y += self.velocity
+        
+    def bounce(self):
+        self.velocity = -self.velocity
+
