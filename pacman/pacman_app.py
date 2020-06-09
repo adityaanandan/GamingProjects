e658a784f648cf1a86303bb106a9d940a6603157
EpicGamerAditya <66402720+EpicGamerAditya@users.noreply.github.com>
2020-06-09 16:49:31 -0700

    Update pacman_app.py

--- pacman/pacman_app.py
+++ pacman/pacman_app.py
@@ -6,9 +6,27 @@ class App:
     def __init__(self):
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         self.load()
+        self.walls = []
+        self.coins = []
+        self.e_pos = []
+        self.p_pos = None
     def load(self): 
         self.background = pygame.image.load('pac background.png')
         self.background = pygame.transform.scale(self.background, (MAZE_WIDTH, MAZE_HEIGHT))
+        with open("walls.txt", 'r') as file:
+            for yidx, line in enumerate(file):
+                for xidx, char in enumerate(line):
+                    if char == "1":
+                        self.walls.append(vec(xidx, yidx))
+                    elif char == "C":
+                        self.coins.append(vec(xidx, yidx))
+                    elif char == "P":
+                        self.p_pos = [xidx, yidx]
+                    elif char in ["2", "3", "4", "5"]:
+                        self.e_pos.append([xidx, yidx])
+                    elif char == "B":
+                        pygame.draw.rect(self.background, BLACK, (xidx*self.cell_width, yidx*self.cell_height,
+                                                                  self.cell_width, self.cell_height))
     def draw(self):
         self.screen.fill(BLACK)
         self.screen.blit(self.background, (TOP_BOTTOM_BUFFER//2, TOP_BOTTOM_BUFFER//2))
