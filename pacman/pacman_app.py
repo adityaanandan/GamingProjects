d7e96019ebb4d67d8f7542b8a446b72576413452
EpicGamerAditya <66402720+EpicGamerAditya@users.noreply.github.com>
2020-06-09 16:01:13 -0700

    Update pacman_app.py

--- pacman/pacman_app.py
+++ pacman/pacman_app.py
@@ -7,9 +7,10 @@ class App:
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         def load(): 
           self.background = pygame.image.load('pacman background.png')
-          self.background = pygame.transform.scale(self.background, WIDTH, HEIGHT)
+          self.background = pygame.transform.scale(self.background, MAZE_WIDTH, MAZE_HEIGHT)
         def playing_draw(self):
           self.screen.fill(BLACK)
           self.screen.blit(self.background, (TOP_BOTTOM_BUFFER//2, TOP_BOTTOM_BUFFER//2))
+          pygame.display.update()
 
     
