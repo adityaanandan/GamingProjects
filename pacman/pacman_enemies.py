48b34da3c16e14054c08ff04e583201144b092e7
EpicGamerAditya <66402720+EpicGamerAditya@users.noreply.github.com>
2020-06-10 18:21:48 -0700

    Update pacman_enemies.py

--- pacman/pacman_enemies.py
+++ pacman/pacman_enemies.py
@@ -1,5 +1,21 @@
 import pygame 
 import sys 
 from settings import* 
+BLUE = (0, 0, 255)
 class Enemy:
+  def __init__(self, app, pos, number): 
+    self.app = app
+    self.grid_pos = pos
+    self.starting_pos = [pos.x, pos.y]
+    self.pix_pos = self.get_pix_pos()
+    self.radius = int(self.app.cell_width//2.3)
+    self.number = number
+    self.color = BLUE
+    self.direction = vec(0, 0)
+    self.personality = self.set_personality()
+    self.target = None
+    self.speed = self.set_speed()
+    def draw(self):
+        pygame.draw.circle(self.app.screen, self.color,
+                           (int(self.pix_pos.x), int(self.pix_pos.y)), self.radius)
 
