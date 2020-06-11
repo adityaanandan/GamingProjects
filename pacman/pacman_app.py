5352eb470811c346fafd65fdae5a91d47be1ea2e
EpicGamerAditya <66402720+EpicGamerAditya@users.noreply.github.com>
2020-06-10 18:00:33 -0700

    Update pacman_app.py

--- pacman/pacman_app.py
+++ pacman/pacman_app.py
@@ -5,36 +5,32 @@ from pacman_settings import*
 class App:
     def __init__(self):
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-        self.load()
+        self.cell_width = MAZE_WIDTH//COLS
+        self.cell_height = MAZE_HEIGHT//ROWS
         self.walls = []
         self.coins = []
         self.e_pos = []
         self.p_pos = None
+        self.load()
     def load(self): 
         self.background = pygame.image.load('pac background.png')
         self.background = pygame.transform.scale(self.background, (MAZE_WIDTH, MAZE_HEIGHT))
-        with open("walls.txt", 'r') as file:
+        with open("wall.txt", 'r') as file:
             for yidx, line in enumerate(file):
                 for xidx, char in enumerate(line):
-                    if char == "1":
-                        self.walls.append(vec(xidx, yidx))
-                    elif char == "C":
+                    if char == "C":
                         self.coins.append(vec(xidx, yidx))
-                    elif char == "P":
-                        self.p_pos = [xidx, yidx]
-                    elif char in ["2", "3", "4", "5"]:
-                        self.e_pos.append([xidx, yidx])
-                    elif char == "B":
                         pygame.draw.rect(self.background, BLACK, (xidx*self.cell_width, yidx*self.cell_height,
                                                                   self.cell_width, self.cell_height))
 
     def draw(self):
         self.screen.fill(BLACK)
         self.screen.blit(self.background, (TOP_BOTTOM_BUFFER//2, TOP_BOTTOM_BUFFER//2))
+        self.draw_coins()
         pygame.display.update()
     def draw_coins(self):
         for coin in self.coins:
             pygame.draw.circle(self.screen, (124, 123, 7),
                                (int(coin.x*self.cell_width)+self.cell_width//2+TOP_BOTTOM_BUFFER//2,
                                 int(coin.y*self.cell_height)+self.cell_height//2+TOP_BOTTOM_BUFFER//2), 5)
-    
+        
